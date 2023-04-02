30294118818478218d3e1eba7570919af04431b4
Piyush Bhudolia <piyushbhudolia786@gmail.com>
2023-04-02 19:28:39 +0530

    _This is my first commit of the project

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const responsive={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+$(document).ready(function(){
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    });
+
+
+//owl -carousel 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    dots:false,
+    nav:true,
+    navText:[$('.owl-nav-prev'),$('.owl-nav-next')],
+    responsive:responsive 
+});
+
+$('.move-up span').click(function(){
+ $('html,body').animate({
+    scrollTop:0
+ },2000);
+
+})
+
+
+});
